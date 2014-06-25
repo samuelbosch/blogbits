cedacf38867f52c35bfc662354df16e3a74f3e9a
Samuel <samuel.bosch+git@gmail.com>
2014-06-25 10:23:35 +0200

    tests where stopped after one iteration, now its slow(er)

--- geosrc/binreaders/bin_reader.py
+++ geosrc/binreaders/bin_reader.py
@@ -21,8 +21,6 @@ def read_values_naive(filename, indices):
                 values.append(None)
     return values
 
-
-
 def bin_to_hdf5(filename, ncells):
     """ convert our binary files to hdf5 files """
     newf = filename.replace('.sbg', '.hdf5')
@@ -32,14 +30,18 @@ def bin_to_hdf5(filename, ncells):
 
 def convert(v):
     if v <> -2147483648:
-        v
+        return v
     else:
-        None
+        return None
 
+cache = {}
 def read_values_hdf5(filename, indices):
-    with tables.open_file(filename) as f:
-        dset = f.root.raster
-        return map(convert, dset[indices])
+    if filename not in cache:
+        f = tables.open_file(filename)
+        cache[filename] = f
+    f = cache[filename]        
+    dset = f.root.raster
+    return map(convert, dset[indices])
 
 def read_values_numpy_memmap(filename, indices):
     values_arr = np.memmap(filename, dtype='int32', mode='r')
@@ -72,16 +74,20 @@ class Test_BinReader(unittest.TestCase):
 
     @timefn
     def read_values_timed(self, reader, repeat, paths, indiceslist):
+        r = []
         for i in xrange(repeat):
             for indices in indiceslist:
                 for path in paths:
-                    return reader(path, indices)
+                    r = reader(path, indices)
+        return r
 
     def test_small_marspec(self):
         testindices = self.load_test_indices()
+        testindices.pop(1000)
+        #testindices.pop(100)
         for outerlen,indiceslist in testindices.iteritems():
-            #print('small marspec %i %i' % (outerlen, len(indiceslist[0])))
-            #self.read_values_timed(read_values_naive, outerlen, [r'D:\temp\bathy_10m.sbg'], indiceslist)
+##            print('small marspec naive %i %i' % (outerlen, len(indiceslist[0])))
+##            self.read_values_timed(read_values_naive, outerlen, [r'D:\temp\bathy_10m.sbg'], indiceslist)
             print('small marspec hdf5 %i %i' % (outerlen, len(indiceslist[0])))
             r = self.read_values_timed(read_values_hdf5, outerlen, [r'D:\temp\bathy_10m.hdf5'], indiceslist)
             print(r[0:10])
@@ -89,16 +95,16 @@ class Test_BinReader(unittest.TestCase):
             r = self.read_values_timed(read_values_numpy_memmap, outerlen, [r'D:\temp\bathy_10m.sbg'], indiceslist)
             print(r[0:10])
             
-    def test_all_marspec(self):
-        paths = glob.glob(r'D:\a\data\marspec\MARSPEC_10m\ascii\*.asc')
-        paths = [os.path.join(r'D:\temp', os.path.split(p)[1].replace('.asc','.sbg')) for p in paths]
-        testindices = self.load_test_indices()
-        for outerlen,indiceslist in testindices.iteritems():
-            print('%i %i numpy memmap all marspec 10m' % (outerlen, len(indiceslist[0])))
-            r = self.read_values_timed(read_values_numpy_memmap, outerlen, paths, indiceslist)
-            print('%i %i hdf5 all marspec 10m' % (outerlen, len(indiceslist[0])))
-            r = self.read_values_timed(read_values_hdf5, outerlen, [p.replace('.sbg','.hdf5') for p in paths], indiceslist)
-         
+##    def test_all_marspec(self):
+##        paths = glob.glob(r'D:\a\data\marspec\MARSPEC_10m\ascii\*.asc')
+##        paths = [os.path.join(r'D:\temp', os.path.split(p)[1].replace('.asc','.sbg')) for p in paths]
+##        testindices = self.load_test_indices()
+##        for outerlen,indiceslist in testindices.iteritems():
+##            print('%i %i numpy memmap all marspec 10m' % (outerlen, len(indiceslist[0])))
+##            r = self.read_values_timed(read_values_numpy_memmap, outerlen, paths, indiceslist)
+##            print('%i %i hdf5 all marspec 10m' % (outerlen, len(indiceslist[0])))
+##            r = self.read_values_timed(read_values_hdf5, outerlen, [p.replace('.sbg','.hdf5') for p in paths], indiceslist)
+##         
 if __name__ == '__main__':
     unittest.main()
     #bin_to_hdf5(r'D:\temp\bathy_10m.sbg', 2160*1080)
