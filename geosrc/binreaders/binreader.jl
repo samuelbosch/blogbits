dc5f06fad3cacef43b69d019e9582b92c0366ac7
Samuel <samuel.bosch+git@gmail.com>
2014-07-08 16:01:20 +0200

    updated julia version, now returns one result set to be able to verify results

--- geosrc/binreaders/binreader.jl
+++ geosrc/binreaders/binreader.jl
@@ -23,15 +23,15 @@ function smallmarspec(outer, inner)
 end
 
 function allmarspec(outer, inner)
-    paths = ["D:\\temp\\bathy_10m.sbg", "D:\\temp\\bathy_10m_plus_300.sbg", "D:\\temp\\biogeo01_aspect_EW_10m.sbg", "D:\\temp\\biogeo02_aspect_NS_10m.sbg", "D:\\temp\\biogeo03_plan_curvature_10m.sbg", "D:\\temp\\biogeo04_profile_curvature_10m.sbg", "D:\\temp\\biogeo05_dist_shore_10m.sbg", "D:\\temp\\biogeo06_bathy_slope_10m.sbg", "D:\\temp\\biogeo07_concavity_10m.sbg", "D:\\temp\\biogeo08_sss_mean_10m.sbg", "D:\\temp\\biogeo09_sss_min_10m.sbg", 
-            "D:\\temp\\biogeo10_sss_max_10m.sbg", "D:\\temp\\biogeo11_sss_range_10m.sbg", "D:\\temp\\biogeo12_sss_variance_10m.sbg", "D:\\temp\\biogeo13_sst_mean_10m.sbg", "D:\\temp\\biogeo14_sst_min_10m.sbg", "D:\\temp\\biogeo15_sst_max_10m.sbg", "D:\\temp\\biogeo16_sst_range_10m.sbg", "D:\\temp\\biogeo17_sst_variance_10m.sbg", "D:\\temp\\sss01_10m.sbg", "D:\\temp\\sss02_10m.sbg", "D:\\temp\\sss03_10m.sbg", "D:\\temp\\sss04_10m.sbg", "D:\\temp\\sss05_10m.sbg", 
-            "D:\\temp\\sss06_10m.sbg", "D:\\temp\\sss07_10m.sbg", "D:\\temp\\sss08_10m.sbg", "D:\\temp\\sss09_10m.sbg", "D:\\temp\\sss10_10m.sbg", "D:\\temp\\sss11_10m.sbg", "D:\\temp\\sss12_10m.sbg", "D:\\temp\\sst01_10m.sbg", "D:\\temp\\sst02_10m.sbg", "D:\\temp\\sst03_10m.sbg", "D:\\temp\\sst04_10m.sbg", "D:\\temp\\sst05_10m.sbg", "D:\\temp\\sst06_10m.sbg", "D:\\temp\\sst07_10m.sbg", "D:\\temp\\sst08_10m.sbg", "D:\\temp\\sst09_10m.sbg"]
+    paths = ["bathy_10m.sbg", "biogeo01_aspect_EW_10m.sbg", "biogeo02_aspect_NS_10m.sbg", "biogeo03_plan_curvature_10m.sbg", "biogeo04_profile_curvature_10m.sbg", "biogeo05_dist_shore_10m.sbg", "biogeo06_bathy_slope_10m.sbg", "biogeo07_concavity_10m.sbg", "biogeo08_sss_mean_10m.sbg", "biogeo09_sss_min_10m.sbg", 
+            "biogeo10_sss_max_10m.sbg", "biogeo11_sss_range_10m.sbg", "biogeo12_sss_variance_10m.sbg", "biogeo13_sst_mean_10m.sbg", "biogeo14_sst_min_10m.sbg", "biogeo15_sst_max_10m.sbg", "biogeo16_sst_range_10m.sbg", "biogeo17_sst_variance_10m.sbg", "sss01_10m.sbg", "sss02_10m.sbg", "sss03_10m.sbg", "sss04_10m.sbg", "sss05_10m.sbg", 
+            "sss06_10m.sbg", "sss07_10m.sbg", "sss08_10m.sbg", "sss09_10m.sbg", "sss10_10m.sbg", "sss11_10m.sbg", "sss12_10m.sbg", "sst01_10m.sbg", "sst02_10m.sbg", "sst03_10m.sbg", "sst04_10m.sbg", "sst05_10m.sbg", "sst06_10m.sbg", "sst07_10m.sbg", "sst08_10m.sbg", "sst09_10m.sbg"]
     indices = getindices(inner)
+    r = []
     for i=1:outer
-        for path in paths
-            r = readvalues(path, indices)
-        end
+        r = [readvalues(string("D:\\temp\\",path), indices) for path in paths]
     end
+    r
 end
 
 @elapsed allmarspec(10,10) # <0.09s
